3b3eed644f19252f1af4eba4803f2fab6a638504
Dexter morgan <naveenvaddavalli@gmail.com>
2025-09-03 19:18:55 -0500

    new commit from vscode

--- nav/src/test/java/com/nav/assertions1.java
+++ nav/src/test/java/com/nav/assertions1.java
@@ -46,36 +46,6 @@ public class assertions1 {
 		
 		
 	}
-	
-	
-	
-
-
-    @Test
-    void testTittle() {
-
-        String exp_title="Opencart";
-        String act_title="Opencart";
-
-        Assert.assertEquals(exp_title,act_title);
-
-    }
-    @Test
-    void testTittlecond() {
-
-        String exp_title="Opencart";
-        String act_title="Opencart";
-        if (exp_title==act_title) {
-            Assert.assertTrue(true);
-        }else {
-            Assert.assertTrue(false);
-        }
-
-
-
-    }
-
-
-
 
 }
+
